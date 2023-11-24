0b6c7d82cb3dfe612b2fc8c90baf0cdf3dba3a9c
Timothy Waite <tim@the-waite.com>
2023-11-24 00:53:12 -0600

    Fix jank config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,6 +2,10 @@
 module.exports = {
   extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
   parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: "module"
+  },
   plugins: ['@typescript-eslint'],
   root: true,
 };
